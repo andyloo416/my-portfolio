9d4c2b9ef6069253b8906380f3277b6842c335d4
andyloo416 <105084947+andyloo416@users.noreply.github.com>
2022-11-17 21:37:39 -0500

    Add files via upload

---
+++ src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Efficient team player with a strong mechanical engineering background.
+        Flexible enough to adapt to the changing environment. Highly motivated
+        individual passionate about mechanical engineering design, piloting and
+        aeronautical engineering. Actively seeking opportunities to learn more
+        and contribute to the industry
+      </p>
+    </div>
+  );
+}
